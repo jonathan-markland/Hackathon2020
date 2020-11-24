9ca4e777dd62e386569ec1dd99ba78ebcc22bd66
Jonathan Markland <jonathan_markland@waters.com>
2020-11-24 22:04:11 +0000

    Milestone with some temporary tracing.

--- MCosmosClassLibrary/FileReader.cs
+++ MCosmosClassLibrary/FileReader.cs
@@ -8,14 +8,14 @@ namespace MCosmosClassLibrary
 {
     public class FileReader
     {
-        private int LineIndex;
+        private int PositionIndex;
         private FileLine[] FileText;
         public readonly string SourceFilePath;
 
         public FileReader(string sourceFile)
         {
             SourceFilePath = sourceFile;
-            LineIndex = 0;
+            PositionIndex = 0;
             var lines = File.ReadAllLines(sourceFile);
             FileText = lines
                 .Select((s, i) => new FileLine { String = s.Trim(), LineNumber = i + 1 })
@@ -23,14 +23,59 @@ namespace MCosmosClassLibrary
                 .ToArray();
         }
 
+        private FileLine Next()
+        {
+            if (PositionIndex < FileText.Length)
+            {
+                return FileText[PositionIndex++];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private FileReader NextLineWhere(Func<string, bool> condition, Func<string> errorMessageGetter)
+        {
+            var errorIndex = PositionIndex;
+
+            for(; ;)
+            {
+                var fileLine = Next();
+
+                if (fileLine != null)
+                {
+                    if (condition(fileLine.String))
+                    {
+                        // TODO: hack:
+                        var traceMsg = $"Scanning from line {FileText[errorIndex].LineNumber}, line {fileLine.LineNumber} ('{fileLine.String}') satisfied the condition.";
+                        System.Diagnostics.Debug.WriteLine(traceMsg);
+
+                        return this;
+                    }
+                }
+                else
+                {
+                    var file = SourceFilePath;
+                    var line = FileText[errorIndex].LineNumber;
+                    var message = errorMessageGetter();
+                    throw new Exception($"{file}({line}): {message}");
+                }
+            }
+        }
+
         public FileReader ExpectLineStartingWith(string leftSideMatchRequired)
         {
-            return this; // TODO
+            return NextLineWhere(
+                s => s.StartsWith(leftSideMatchRequired), 
+                () => $"Could not find a line beginning with '{leftSideMatchRequired}' from this point forwards.");
         }
 
         public FileReader ExpectWholeLine(string lineContentRequired)
         {
-            return this; // TODO
+            return NextLineWhere(
+                s => String.Compare(s, lineContentRequired) == 0,
+                () => $"Could not find a line containing '{lineContentRequired}' from this point forwards.");
         }
 
         public FileReader Parameter<T>(string leftSideMatchRequired, Func<string, T?> parser, out T result) where T:struct
@@ -46,9 +91,11 @@ namespace MCosmosClassLibrary
                 throw new Exception($"Failed to read value after '{leftSideMatchRequired}'."); // TODO: message according to parser type
             }
         }
+
+        
     }
 
-    public struct FileLine
+    internal class FileLine
     {
         public string String;
         public int LineNumber;
