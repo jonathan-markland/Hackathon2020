b3fd2425d493963d6b462d029cea2481a7792bfe
Jonathan Markland <jonathan_markland@waters.com>
2020-11-24 22:52:09 +0000

    Updates

--- MCosmosClassLibrary/FileReader.cs
+++ MCosmosClassLibrary/FileReader.cs
@@ -25,14 +25,7 @@ namespace MCosmosClassLibrary
 
         private FileLine Next()
         {
-            if (PositionIndex < FileText.Length)
-            {
-                return FileText[PositionIndex++];
-            }
-            else
-            {
-                return null;
-            }
+            return (PositionIndex < FileText.Length) ? FileText[PositionIndex++] : null;
         }
 
         private FileReader NextLineWhere(
@@ -40,30 +33,56 @@ namespace MCosmosClassLibrary
             Func<string> errorMessageGetter, 
             Action<string> applyActionForRowFound)
         {
-            var errorIndex = PositionIndex;
-
-            for(; ;)
-            {
-                var fileLine = Next();
-
-                if (fileLine != null)
+            WithFileAndLineNumberExceptionReporting(
+                () => 
                 {
-                    if (condition(fileLine.String))
-                    {
-                        // TODO: hack:
-                        var traceMsg = $"Scanning from line {FileText[errorIndex].LineNumber}, line {fileLine.LineNumber} ('{fileLine.String}') satisfied the condition.";
-                        System.Diagnostics.Debug.WriteLine(traceMsg);
+                    var traceIndex = PositionIndex; // TODO: remove
 
-                        applyActionForRowFound(fileLine.String);
-                        return this;
+                    for (; ; )
+                    {
+                        var fileLine = Next();
+
+                        if (fileLine != null)
+                        {
+                            if (condition(fileLine.String))
+                            {
+                                // TODO: hack:
+                                var traceMsg = $"Scanning from line {FileText[traceIndex].LineNumber}, line {fileLine.LineNumber} ('{fileLine.String}') satisfied the condition.";
+                                System.Diagnostics.Debug.WriteLine(traceMsg);
+
+                                applyActionForRowFound(fileLine.String);
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception(errorMessageGetter());
+                        }
                     }
+                });
+
+            return this;
+        }
+
+        private void WithFileAndLineNumberExceptionReporting(Action a)
+        {
+            var bailoutErrorIndex = PositionIndex;
+            try
+            {
+                a();
+            }
+            catch (Exception e)  // TODO: specialise the type
+            {
+                var file = SourceFilePath;
+                var index = PositionIndex < FileText.Length ? PositionIndex : bailoutErrorIndex;
+                if (index < FileText.Length)
+                {
+                    var line = FileText[index].LineNumber;
+                    throw new Exception($"{file}({line}): {e.Message}");
                 }
                 else
                 {
-                    var file = SourceFilePath;
-                    var line = FileText[errorIndex].LineNumber;
-                    var message = errorMessageGetter();
-                    throw new Exception($"{file}({line}): {message}");
+                    throw new Exception($"{file}: {e.Message}");
                 }
             }
         }
@@ -86,15 +105,15 @@ namespace MCosmosClassLibrary
 
         public FileReader Parameter(string leftSideMatchRequired, Func<string, double?> parser, out double result)
         {
-            var tempSlot = new Box<double>();  // because we can't lambda-bind an 'out' parameter.   TODO: Is there a more C#-native solution?
+            var box = new Box<double>();  // because we can't lambda-bind an 'out' parameter.   TODO: Is there a more C#-native solution?
 
-            void TryParseNumber(string documentRow)
+            void TryExtractDataFromLineFound(string documentRow)
             {
                 var rightHandSide = documentRow.Substring(leftSideMatchRequired.Length);
                 var parseResult = parser(rightHandSide);
                 if (parseResult.HasValue)
                 {
-                    tempSlot.BoxContent = parseResult.Value;
+                    box.Content = parseResult.Value;
                 }
                 else
                 {
@@ -105,16 +124,16 @@ namespace MCosmosClassLibrary
             var retVal = NextLineWhere(
                 s => s.StartsWith(leftSideMatchRequired),
                 () => $"Could not find a line beginning with '{leftSideMatchRequired}' from this point forwards.",
-                TryParseNumber);
+                TryExtractDataFromLineFound);
 
-            result = tempSlot.BoxContent;
+            result = box.Content;
             return retVal;
         }
     }
 
-    public class Box<T>
+    internal class Box<T>
     {
-        public T BoxContent;
+        public T Content;
     }
 
 
