fe74d92bfde83f9af7c3102fec716615cd79304a
Jonathan Markland <jonathan_markland@waters.com>
2020-11-25 14:40:15 +0000

    CSV row

---
+++ MCosmosClassLibrary/CsvGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCosmosClassLibrary
+{
+    public static class CsvGenerator
+    {
+        public static string Headings
+        {
+            get { return "\"flatDatumF\",\"flatDatumE\",\"flatDatumD\",\"flatDatumG\",\"paraDatumELH1\",\"paraDatumERH1\",\"paraDatumGFR1\",\"paraDatumGBK1\",\"distEtoFLeft1\",\"distEtoFRight1\",\"distEtoFLeft2\",\"distEtoFRight2\",\"distGtoDFront1\",\"distGtoDBack1\",\"distGtoDFront2\",\"distGtoDBack2\""; }
+        }
+
+        public static string GetLine(DiscInfo discInfo)
+        {
+            var serialNo       = discInfo.Metadata.SerialNo;
+            var flatDatumF     = discInfo.Flatness.DatumF;
+            var flatDatumE     = discInfo.Flatness.DatumE;
+            var flatDatumD     = discInfo.Flatness.DatumD;
+            var flatDatumG     = discInfo.Flatness.DatumG;
+            var paraDatumELH1  = discInfo.Parallel.DatumELH1;
+            var paraDatumERH1  = discInfo.Parallel.DatumERH1;
+            var paraDatumGFR1  = discInfo.Parallel.DatumGFR1;
+            var paraDatumGBK1  = discInfo.Parallel.DatumGBK1;
+            var distEtoFLeft1  = discInfo.Distances.EtoFLeft1;
+            var distEtoFRight1 = discInfo.Distances.EtoFRight1;
+            var distEtoFLeft2  = discInfo.Distances.EtoFLeft2;
+            var distEtoFRight2 = discInfo.Distances.EtoFRight2;
+            var distGtoDFront1 = discInfo.Distances.GtoDFront1;
+            var distGtoDBack1  = discInfo.Distances.GtoDBack1;
+            var distGtoDFront2 = discInfo.Distances.GtoDFront2;
+            var distGtoDBack2  = discInfo.Distances.GtoDBack2;
+            return $"\"{serialNo}\",\"{flatDatumF:0.00000}\",\"{flatDatumE:0.00000}\",\"{flatDatumD:0.00000}\",\"{flatDatumG:0.00000}\",\"{paraDatumELH1:0.00000}\",\"{paraDatumERH1:0.00000}\",\"{paraDatumGFR1:0.00000}\",\"{paraDatumGBK1:0.00000}\",\"{distEtoFLeft1:0.00000}\",\"{distEtoFRight1:0.00000}\",\"{distEtoFLeft2:0.00000}\",\"{distEtoFRight2:0.00000}\",\"{distGtoDFront1:0.00000}\",\"{distGtoDBack1:0.00000}\",\"{distGtoDFront2:0.00000}\",\"{distGtoDBack2:0.00000}\"";
+        }
+    }
+}
---
+++ TestProject/TestCSV.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Xunit;
+using MCosmosClassLibrary;
+
+namespace TestProject
+{
+    public class TestCSV
+    {
+        [Fact]
+        public void SerialiseDiscToCSVPassesFieldsThroughInExpectedOrder()
+        {
+            var metadata = new DiscMetadata {
+                SerialNo = "123"
+            };
+
+            var flatness = new FlatnessMeasurements
+            {
+                DatumF = 1.0,
+                DatumE = 2.0,
+                DatumD = 3.0,
+                DatumG = 4.0,
+            };
+
+            var parallel = new ParallelMeasurements
+            {
+                DatumELH1 = 5.0,
+                DatumERH1 = 6.0,
+                DatumGFR1 = 7.0,
+                DatumGBK1 = 8.0,
+            };
+
+            var distances = new DistanceMeasurements
+            {
+                EtoFLeft1  = 9.0,
+                EtoFRight1 = 10.0,
+                EtoFLeft2  = 11.0,
+                EtoFRight2 = 12.0,
+                GtoDFront1 = 13.0,
+                GtoDBack1  = 14.0,
+                GtoDFront2 = 15.0,
+                GtoDBack2  = 16.0,
+            };
+
+            var discInfo = new DiscInfo
+            {
+                Metadata  = metadata,
+                Flatness  = flatness,
+                Parallel  = parallel,
+                Distances = distances
+            };
+
+            var actual = CsvGenerator.GetLine(discInfo);
+            var expectation = "\"123\",\"1.00000\",\"2.00000\",\"3.00000\",\"4.00000\",\"5.00000\",\"6.00000\",\"7.00000\",\"8.00000\",\"9.00000\",\"10.00000\",\"11.00000\",\"12.00000\",\"13.00000\",\"14.00000\",\"15.00000\",\"16.00000\"";
+            Assert.Equal(expectation, actual);
+        }
+    }
+}
