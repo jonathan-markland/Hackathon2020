9a94cef0a4b89f69f65a8a63f33b2c2db1e108c1
Jonathan Markland <jonathan.markland@btinternet.com>
2020-12-22 19:34:02 +0000

    Folder watcher

---
+++ HackathonSln/MCosmosFolderWatcher/Algorithms/BatchFolderLoader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using MCosmosClassLibrary;
+using MCosmosFolderWatcher.Models;
+
+namespace MCosmosFolderWatcher.Algorithms
+{
+    public class BatchFolderLoader
+    {
+        public BatchBase LoadDiscsFromFolder(string pathToFolder)
+        {
+            try
+            {
+                return LoadDiscsFromFolder2(pathToFolder);
+            }
+            catch(Exception ex)
+            {
+                return new BatchOverallError
+                { 
+                    PathToFolder = pathToFolder, 
+                    OverallErrorMessage = ex.Message 
+                };
+            }
+        }
+
+
+        private BatchBase LoadDiscsFromFolder2(string pathToFolder)
+        {
+            // A best-effort returning error detail for individual files.
+
+            var discs = new List<DiscInfo>();
+            var errors = new List<FileProcessingError>();
+
+            foreach(var filePath in Directory.GetFiles(pathToFolder))
+            {
+                try
+                {
+                    var d = DiscFileLoader.LoadDiscFromFile(filePath);
+                    discs.Add(d);
+                }
+                catch(Exception ex) // TODO: more specific?
+                {
+                    var e = new FileProcessingError
+                    {
+                        Error = ex.Message,
+                        PathToErrantFile = filePath
+                    };
+                    errors.Add(e);
+                }
+            }
+
+            return new Batch { Discs = discs, FileProcessingErrors = errors };
+        }
+    }
+}
---
+++ HackathonSln/MCosmosFolderWatcher/Models/Batch.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MCosmosClassLibrary;
+
+namespace MCosmosFolderWatcher.Models
+{
+    /// <summary>
+    /// Base class for batch folder processing results.
+    /// </summary>
+    public class BatchBase
+    {
+        // Nothing to put here yet.
+
+        /// <summary>
+        /// The folder on which the load was attempted.
+        /// </summary>
+        public string PathToFolder { get; init; }
+    }
+
+    /// <summary>
+    /// Batch failed because of a very fundamental single reason.
+    /// </summary>
+    public class BatchOverallError: BatchBase
+    {
+        public string OverallErrorMessage { get; init; }
+    }
+
+    /// <summary>
+    /// A batch of Alignment Disc files loaded from a folder, and failure information.
+    /// </summary>
+    public class Batch: BatchBase
+    {
+        /// <summary>
+        /// Files successfully loaded and parsed as discs
+        /// </summary>
+        public List<DiscInfo> Discs;
+
+        /// <summary>
+        /// If non-empty, is the file processing errors of individual files.
+        /// </summary>
+        public List<FileProcessingError> FileProcessingErrors;
+    }
+
+    /// <summary>
+    /// If processing of a single file in the batch folder failed, this is created.
+    /// </summary>
+    public class FileProcessingError
+    {
+        /// <summary>
+        /// The path to the file that had a processing error.
+        /// </summary>
+        public string PathToErrantFile;
+
+        /// <summary>
+        /// The error message text.
+        /// </summary>
+        public string Error;
+    }
+}
