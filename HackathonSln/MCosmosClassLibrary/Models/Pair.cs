8792f62cc3a0df7a8d4628d6bf51d1ef18f4c888
Jonathan Markland <jonathan.markland@btinternet.com>
2020-12-25 12:46:04 +0000

    Move Pair class out to Models namespace.

---
+++ HackathonSln/MCosmosClassLibrary/Models/Pair.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCosmosClassLibrary.Models
+{
+    /// <summary>
+    /// A matched pair of discs, including all related information.
+    /// </summary>
+    public class Pair
+    {
+        public Pair(DiscInfo disc1, DiscInfo disc2)
+        {
+            Disc1 = disc1;
+            Disc2 = disc2;
+            EuclideanDistance =
+                PairingMathematics.EuclideanDistanceBetween(
+                    disc1.Distances, disc2.Distances);
+        }
+
+        public readonly DiscInfo Disc1;
+        public readonly DiscInfo Disc2;
+        public readonly double EuclideanDistance;
+    }
+}
--- HackathonSln/MCosmosClassLibrary/PairingMathematics.cs
+++ HackathonSln/MCosmosClassLibrary/PairingMathematics.cs
@@ -64,23 +64,4 @@ namespace MCosmosClassLibrary
             }
         }
     }
-
-    /// <summary>
-    /// A matched pair of discs, including all related information.
-    /// </summary>
-    public class Pair
-    {
-        public Pair(DiscInfo disc1, DiscInfo disc2)
-        {
-            Disc1 = disc1;
-            Disc2 = disc2;
-            EuclideanDistance =
-                PairingMathematics.EuclideanDistanceBetween(
-                    disc1.Distances, disc2.Distances);
-        }
-
-        public readonly DiscInfo Disc1;
-        public readonly DiscInfo Disc2;
-        public readonly double EuclideanDistance;
-    }
 }
