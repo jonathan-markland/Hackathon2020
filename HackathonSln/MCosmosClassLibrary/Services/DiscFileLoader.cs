55e12e9fb77650ff750641582b16c1fc664c62b9
Jonathan Markland <jonathan.markland@btinternet.com>
2021-03-18 19:09:20 +0000

    Tidy

--- HackathonSln/MCosmosClassLibrary/Services/DiscFileLoader.cs
+++ HackathonSln/MCosmosClassLibrary/Services/DiscFileLoader.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GeneralClassLibrary;
+﻿using GeneralClassLibrary;
 using MCosmosClassLibrary.Models;
 
 namespace MCosmosClassLibrary.Services
